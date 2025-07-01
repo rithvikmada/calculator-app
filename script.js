706bc724e0f71be3db8de124aaae56519b87a6a7
Rithvik Mada <73190672+rithvikmada@users.noreply.github.com>
2025-06-30 21:55:13 -0400

    Build operate() function to perform calculation
    - Declare "number1", "operator", and "number2" variables
    - Define logic to assess input operator and perform respective
    calculation
    - Return output to local "value" variable

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
     return number1 / number2;
 }
+
+let number1;
+let operator;
+let number2;
+
+function operate(number1, operator, number2) {
+    let value;
+
+    if (operator == "+") {
+        value = add(number1, number2);
+    } else if (operator == "-") {
+        value = subtract(number1, number2);
+    } else if (operator == "*") {
+        value = multiply(number1, number2);
+    } else if (operator == "/") {
+        value = divide(number1, number2);
+    }
+
+    return value;
+}
