26a3262972837b39cea075bc236ebaf195645161
Rithvik Mada <73190672+rithvikmada@users.noreply.github.com>
2026-06-20 23:36:04 -0400

    Create operate() function
    - Initialize param1, param2, & operator variables
    - Define operate function() to call respective math function

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function divide(param1, param2) {
         return param1 / param2;
     }
 }
+
+let param1;
+let param2;
+let operator;
+
+function operate(param1, param2, operator) {
+    switch (operator) {
+        case "+":
+            return sum(param1, param2);
+            break;
+        case "-":
+            return diff(param1, param2);
+            break;
+        case "*":
+            return multiply(param1, param2);
+            break;
+        case "/":
+            return divide(param1, param2);
+            break;
+    }
+}
